78e26fcdbdb4084494d5771dee6a34fd5dfe37af
Philip Heur <heops79@gmai.com>
2021-05-13 00:09:20 +0900

    Binary Tree done and BST deletion doing

--- tree/Tree.js
+++ tree/Tree.js
@@ -1,3 +1,5 @@
+const readlineSync = require('readline-sync');
+
 
 // tail  ----->   head
 class Queue {
@@ -99,10 +101,111 @@ class BinaryTree {
   }
 
   create() {
-    
+    const queue = new Queue();
+    let input = readlineSync.question("Value of Root: ");
+    this.root = new TreeNode(+input);
+    queue.enqueue(this.root);
+
+    let cur = null;
+    while (!queue.isEmpty()) {
+      cur = queue.dequeue();
+      input = readlineSync.question(`Left value of ${cur.data}: `);
+      if (+input !== -1) {
+        cur.left = new TreeNode(+input);
+        queue.enqueue(cur.left);
+      }
+
+      input = readlineSync.question(`Right value of ${cur.data}: `);
+      if (+input !== -1) {
+        cur.right = new TreeNode(+input);
+        queue.enqueue(cur.right);
+      }
+    }
   }
 
+  inorderRecursive(node) {
+    if (node === null) return;
+
+    this.inorderRecursive(node.left);
+    console.log(node.data);
+    this.inorderRecursive(node.right);
+  }
+
+  inorderIterative() {
+    let cur = this.root;
+    const stack = new Stack();
+
+    while (cur !== null || !stack.isEmpty()) {
+      if (cur !== null) {
+        stack.push(cur);
+        cur = cur.left;
+      } else {
+        cur = stack.pop();
+        console.log(cur.data);
+        cur = cur.right;
+      }
+    }
+  }
+
+  preorderRecursive(node) {
+    if (node === null) return;
+
+    console.log(node.data);
+    this.preorderRecursive(node.left);
+    this.preorderRecursive(node.right);
+  }
+
+  preorderIterative() {
+    let cur = this.root;
+    const stack = new Stack();
+
+    while (cur !== null || !stack.isEmpty()) {
+      if (cur !== null) {
+        console.log(cur.data);
+        stack.push(cur);
+        cur = cur.left;
+      } else {
+        cur = stack.pop();
+        cur = cur.right;
+      }
+    }
+  }
+
+  postorderRecursive(node) {
+    if (node === null) return;
+
+    this.postorderRecursive(node.left);
+    this.postorderRecursive(node.right);
+    console.log(node.data);
+  }
+
+  postorderIterative() {
+    let cur = this.root;
+    let node = null;
+    const stack = new Stack();
+
+    while (cur !== null || !stack.isEmpty()) {
+      if (cur !== null) {
+        stack.push({cur, order: 1});
+        cur = cur.left;
+      } else {
+        node = stack.pop();
+        cur = node.cur;
+
+        if (node.order === 1) {
+          stack.push({cur, order: 2});
+          cur = cur.right;
+        } else if (node.order === 2) {
+          console.log(cur.data);
+          // cur has to set to be null. because if the node has taken second time, 
+          // cur is directing some node. not null
+          cur = null;
+        }
+      }
+    }
+  }
 }
+
 
 class TreeNode {
   constructor(data) {
@@ -111,3 +214,85 @@ class TreeNode {
     this.right = null;
   }
 }
+
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(node, data) {
+    if (node === null) {
+      return new TreeNode(data);
+    }
+
+    if (data > node.data) {
+      node.right = this.insert(node.right, data);
+    } else {
+      node.left = this.insert(node.left, data);
+    }
+    return node;
+  }
+
+  search(node, key) {
+    if (node === null) return;
+
+    if (node.data === key) {
+      return node;
+    } else if (node.data > key) {
+      return this.search(node.left, key);
+    } else if (node.data < key) {
+      return this.search(node.right, key);
+    }
+  }
+
+  getHeight(node) {
+    if (node === null) return 0;
+
+    const leftHeight = this.getHeight(node.left);
+    const rightHeight = this.getHeight(node.right);
+
+    return leftHeight > rightHeight ? leftHeight + 1 : rightHeight + 1;
+  }
+
+  delete(node, key) {
+    if (node === null) return;
+
+    if (node.data === key) {
+
+    } else if (node.data > key) {
+      this.delete(node.left);
+    } else if (node.data < key) {
+      this.delete(node.right);
+    }
+  }
+}
+
+const myBST = new BinarySearchTree();
+myBST.root = myBST.insert(myBST.root, 20);
+myBST.insert(myBST.root, 10);
+myBST.insert(myBST.root, 30);
+myBST.insert(myBST.root, 15);
+myBST.insert(myBST.root, 5);
+myBST.insert(myBST.root, 35);
+
+console.log(JSON.stringify(myBST));
+console.log(myBST.search(myBST.root, 11));
+console.log(myBST.getHeight(myBST.root));
+
+
+// const myTree = new BinaryTree();
+// myTree.create();
+
+// console.log("--- inorder ---")
+// myTree.inorderRecursive(myTree.root);
+// console.log("--- preorder ---")
+// myTree.preorderRecursive(myTree.root);
+// console.log("--- postorder ---")
+// myTree.postorderRecursive(myTree.root);
+// console.log("-- inorder iterative ---")
+// myTree.inorderIterative();
+// console.log("-- preorder iterative ---")
+// myTree.preorderIterative();
+// console.log("-- postorder iterative ---")
+// myTree.postorderIterative();
