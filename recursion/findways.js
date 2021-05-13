b91cb298a102b13916232660b94e3381d03d23cc
Philip Heur <heops79@gmai.com>
2021-05-13 23:59:13 +0900

    just counted

--- recursion/findways.js
+++ recursion/findways.js
@@ -1,10 +1,9 @@
 const readlineSync = require('readline-sync');
-
+let global = 0;
 
 function findways(cur, target) {
-  
+  global++;
   if (cur === target) {
-    console.log(cur, target);
     return 1;
   }
   if (cur > target) return 0;
@@ -16,3 +15,4 @@ function findways(cur, target) {
 let steps = readlineSync.question("How many steps: ");
 const answer = findways(0, +steps);
 console.log(answer);
+console.log(global)
